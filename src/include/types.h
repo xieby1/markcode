912e69e0131fe358ca27c65a7a5b0d33f2a3732c
xieby1 <xieby1@outlook.com>
2023-11-24 19:53:04 +0800

    fix only space allow after `MC`, not \s, which including newline

--- src/include/types.h
+++ src/include/types.h
@@ -19,10 +19,10 @@ TSLanguage *tree_sitter_nix();
 static const Type types[] = {
 //MC{visible}
 //MC ## Patterns
-    {"bash",    tree_sitter_bash,   {"sh", NULL},                                  "\\s*#+MC({(?<cf>[^}]*)})?(\\s?(?<md>.*\\n))"},
-    {"c",       tree_sitter_c,      {"c", "h", NULL},                             "\\s*//+MC({(?<cf>[^}]*)})?(\\s?(?<md>.*\\n))"},
-    {"cpp",     tree_sitter_cpp,    {"cc", "cpp", "hh", "hpp", NULL},             "\\s*//+MC({(?<cf>[^}]*)})?(\\s?(?<md>.*\\n))"},
-    {"nix",     tree_sitter_nix,    {"nix", NULL},                                 "\\s*#+MC({(?<cf>[^}]*)})?(\\s?(?<md>.*\\n))"},
+    {"bash",    tree_sitter_bash,   {"sh", NULL},                                  "\\s*#+MC({(?<cf>[^}]*)})?( ?(?<md>.*\\n))"},
+    {"c",       tree_sitter_c,      {"c", "h", NULL},                             "\\s*//+MC({(?<cf>[^}]*)})?( ?(?<md>.*\\n))"},
+    {"cpp",     tree_sitter_cpp,    {"cc", "cpp", "hh", "hpp", NULL},             "\\s*//+MC({(?<cf>[^}]*)})?( ?(?<md>.*\\n))"},
+    {"nix",     tree_sitter_nix,    {"nix", NULL},                                 "\\s*#+MC({(?<cf>[^}]*)})?( ?(?<md>.*\\n))"},
 };
 #define types_len sizeof(types) / sizeof(Type)
 
